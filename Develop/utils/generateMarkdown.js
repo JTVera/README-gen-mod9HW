879ae177258f218d47fb6e705760a45229a3848a
JTVera <112497419+JTVera@users.noreply.github.com>
2023-01-20 18:08:45 -0600

    finished markdowns

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,74 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(answers) {
+  let importBadge = ""
+  if(answers.license ==="None"){
+    importBadge = ""
+    return importBadge;
+  }else{
+    return `![badge](https://img.shields.io/badge/license-${answers.license}-blue)<br />`
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseSection(answers) {
+  if (answers.license === "None") {
+    return "";
+  } else {
+    return   `${answers.license}`
+    }
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function generateMarkdown(answers) {
+  
+  const licenseLinks = {
+    MIT: "https://opensource.org/licenses/MIT",
+    BSD3clause: "https://opensource.org/licenses/BSD-3-Clause",
+    Apache2: "http://www.apache.org/licenses/LICENSE-2.0",
+    GPLv3: "https://www.gnu.org/licenses/gpl-3.0.html",
+    GPLv2: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html",
+    None: ""
+  };
+  answers.licenseLink = licenseLinks[answers.license];
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# **${answers.title}**
 
-`;
+## [${answers.user}](https://github.com/${answers.user}) 👋
+___
+${renderLicenseBadge(answers)}
+
+ # Table of Contents 📒
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+___
+  ## Description 
+  ${answers.description}
+___
+  ## Installation 
+  ${answers.installation}
+___
+  ## Usage 
+  ${answers.usage}
+___
+  ## Contribution 
+  ${answers.contribution}
+___
+  
+  ## Tests  
+  ${answers.tests}
+  
+___
+  ## License 
+[${renderLicenseSection(answers)}](${answers.licenseLink})
+___
+  ### Questions ❓
+  Contact me with any additional questions:
+  <br />
+  💻 ${answers.email}
+`
+
+;
 }
 
 module.exports = generateMarkdown;
